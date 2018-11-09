431fd34e11d8e70a2ab9d1a1b04ef97e04c6b957
Matt <12matQ@gmail.com>
2018-11-09 23:41:27 +0100

    Added modes
    Advanced mode doesn't work yet. Wait for fixes.
    
    Basic should work as inteded.

--- generator.py
+++ generator.py
@@ -3,12 +3,45 @@ try:
 except:
     file = open("output.txt", "x")
 
-while True:
+mode = input("Select mode ('a' for advanced, 'b' for basic, 'h' for help): ")
+
+if mode == "help":
+    print("--------------------------")
+    print("Advanced mode means that you are prompt for both 'full star' and 'empty star' symbol (Or whatever else you type in) and also your max. skill points count.")
+    print("Basic mode means that generated code will contain simple empty/full star rating.")
+    print("--------------------------")
+elif mode == "a":
+    starOff = input("'Empty star': ")
+    starOn = input("'Full star': ")
+
+while mode == "a":
     print("Type 'exit' to close program.")
     name = input("Skill name: ")
     if name == "exit":
         exit(0)
         file.close()
+    maxSkill = input("Max. symbols count: ")
+    msg = "Rating (1-" + str(maxSkill) + "): "
+    level = input(msg)
+    
+    output = name + " "
+    for x in range(0, int(maxSkill)+1):
+        if x <= int(level):
+            output += starOn
+        elif x > int(level) and x < int(maxSkill):
+            output += starOff
+        elif x == int(maxSkill):
+            output += "<br/>" + "\n"
+        
+    file.write(str(output))
+
+while mode == "b":
+    print("Type 'exit' to close program.")
+    name = input("Skill name: ")
+    if name == "exit":
+        exit(0)
+        file.close()
+
 
     level = int(input("Rating (1-3): "))
     if level == 1:
