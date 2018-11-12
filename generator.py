c67bf8bf279ca0e406868d727faeefcb347b2e4e
Matt <12matQ@gmail.com>
2018-11-12 14:14:07 +0100

    Update generator.py

--- generator.py
+++ generator.py
@@ -1,73 +1,27 @@
 import tkinter
 from tkinter import messagebox
 
+text = "0"
+
 top = tkinter.Tk()
-top.geometry("100x100")
+top.geometry("500x200")
 def helloCallBack():
    msg = messagebox.showinfo( "Hello Python", inputEmptyStar.get())
 
-B = tkinter.Button(top, text = "Hello", command = helloCallBack)
-B.place(x = 50,y = 50)
+def updateTable():
+    text = count.get()
+    for c in range(0,int(text)):
+        tkinter.Label(text=c, relief=tkinter.RIDGE,width=15).grid(row=c+5,column=0)
+        tkinter.Entry(relief=tkinter.SUNKEN,width=10).grid(row=c+5,column=1)
 
-labelEmptyStar = tkinter.Label(top, text="Empty star symbol: ")
-labelEmptyStar.pack(side=tkinter.LEFT)
-inputEmptyStar = tkinter.Entry(top, bd=5)
-inputEmptyStar.pack(side=tkinter.RIGHT)
+tkinter.Label(text="Empty Star: ", relief=tkinter.RIDGE,width=20).grid(row=1,column=0)
+tkinter.Entry(bg="yellow", relief=tkinter.SUNKEN,width=30).grid(row=1,column=1)
+tkinter.Label(text="Full Star: ", relief=tkinter.RIDGE,width=20).grid(row=2,column=0)
+tkinter.Entry(bg="yellow", relief=tkinter.SUNKEN,width=30).grid(row=2,column=1)
+tkinter.Label(text="Max skill points: ", relief=tkinter.RIDGE,width=20).grid(row=3,column=0)
+count = tkinter.Entry(bg="green", relief=tkinter.SUNKEN,width=30, textvariable=text)
+count.grid(row=3,column=1)
+tkinter.Button(text="Update!", bg="green", relief=tkinter.SUNKEN,width=30, command=updateTable).grid(row=3, column=2)
+
 
 top.mainloop()
-
-try:
-    file = open("output.txt", "a")
-except:
-    file = open("output.txt", "x")
-
-mode = input("Select mode ('a' for advanced, 'b' for basic, 'h' for help): ")
-
-if mode == "help":
-    print("--------------------------")
-    print("Advanced mode means that you are prompt for both 'full star' and 'empty star' symbol (Or whatever else you type in) and also your max. skill points count.")
-    print("Basic mode means that generated code will contain simple empty/full star rating.")
-    print("--------------------------")
-elif mode == "a":
-    starOff = input("'Empty star': ")
-    starOn = input("'Full star': ")
-
-while mode == "a":
-    print("Type 'exit' to close program.")
-    name = input("Skill name: ")
-    if name == "exit":
-        exit(0)
-        file.close()
-    maxSkill = input("Max. symbols count: ")
-    msg = "Rating (1-" + str(maxSkill) + "): "
-    level = input(msg)
-    
-    output = name + " "
-    for x in range(0, int(maxSkill)+1):
-        if x <= int(level):
-            output += starOn
-        elif x > int(level) and x < int(maxSkill):
-            output += starOff
-        elif x == int(maxSkill):
-            output += "<br/>" + "\n"
-        
-    file.write(str(output))
-
-while mode == "b":
-    print("Type 'exit' to close program.")
-    name = input("Skill name: ")
-    if name == "exit":
-        exit(0)
-        file.close()
-
-
-    level = int(input("Rating (1-3): "))
-    if level == 1:
-        output = name + " " + "&#9733;" + "&#9734;" + "&#9734;" + "<br/>" + "\n"
-    elif level == 2:
-        output = name + " " + "&#9733;" + "&#9733;" + "&#9734;" + "<br/>" + "\n"
-    elif level == 3:
-        output = name + " " + "&#9733;" + "&#9733;" + "&#9733;" + "<br/>" + "\n"
-    else:
-        print("Invalid rating number")
-    file.write(str(output))
